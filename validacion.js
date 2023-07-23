058bdd6d33c55c481859fe8e1393e3580a38e507
sebastian <sebastianleon5303@gmail.com>
2023-07-23 01:56:02 -0500

    Se implementa el formulario definitivo y el enlace con gmail validado

--- validacion.js
+++ validacion.js
@@ -38,44 +38,41 @@ $(document).ready(function () {
         var isMessageValid = validateField("#texcomentario", /^[\w\s]{5,}$/);
         var isEmailValid = validateField("#texCorreo", /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
         
-
         var isValid = isNameValid && isPhoneValid && isMessageValid && isEmailValid;
 
         if (isValid) {
-           
             var form = document.getElementById("my-form");
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          status.innerHTML = "Thanks for your submission!";
-          form.reset()
+            var status = document.getElementById("my-form-status");
+            var data = new FormData(form);
+            
+            fetch(form.action, {
+                method: form.method,
+                body: data,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            }).then(response => {
+                if (response.ok) {
+                    alert("Formulario satisfactorio")
+                    $("#Nombresyapellidos").val("");
+                    $("#Numerocelular").val("");
+                    $("#texcomentario").val("");
+                    $("#texCorreo").val("");
+                    form.reset()
+                } else {
+                    response.json().then(data => {
+                        if (Object.hasOwn(data, 'errors')) {
+                            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                        } else {
+                            status.innerHTML = "Oops! There was a problem submitting your form"
+                        }
+                    })
+                }
+            }).catch(error => {
+                status.innerHTML = "Oops! There was a problem submitting your form"
+            });
+            
         } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-              status.innerHTML = "Oops! There was a problem submitting your form"
-            }
-          })
-        }
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-      });
-    }
-    form.addEventListener("submit", handleSubmit)
-
-        } 
-        else {
             alert("Formulario no válido. Por favor, corrige los errores.");
         }
     });
